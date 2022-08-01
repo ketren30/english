79b208ab4abd9e721c094222176819bc8188ab40
ekaterina <uragan.ketren@gmail.com>
2022-08-01 22:51:34 +0300

    ggd

--- форма регистрации/components/input/input.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Inp = (prop) => {
-    return (
-        <input onChange={prop.onHandleChange} onBlur={prop.onBlurChange}/>
-    )
-}
--- форма регистрации/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { Main } from './components/main/main';
-ReactDOM.render(<Main title='Регистрация' />, document.getElementById("root"));
