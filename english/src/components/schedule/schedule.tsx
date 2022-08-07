eac9bb0a769a1051a9f82b687b1664a0e8adc935
ekaterina <uragan.ketren@gmail.com>
2022-08-07 14:04:33 +0300

    dffd

--- english/src/components/schedule/schedule.tsx
+++ english/src/components/schedule/schedule.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import { time } from 'console';
+import React, { useEffect, useState } from 'react';
 
-type Props = {
-    name?: string
+interface Lesson {
+    teacher: string
+    level: string,
+    "number of students": number
 }
-export const Schedule: React.FC<Props> = ({name}) => {
+interface Time {
+    Monday: Lesson,
+        Tuesday: Lesson,
+        Wednesday: Lesson,
+        Thursday: Lesson,
+        Friday: Lesson,
+        Saturday: Lesson
+}
+interface Classroom {
+    "09-00": Time,
+    "10-00": Time,
+    "11-00": Time,
+    "12-00": Time,
+    "13-00": Time,
+    "14-00": Time,
+    "15-00": Time,
+    "16-00": Time,
+    "17-00": Time,
+    "18-00": Time,
+}
+export const Schedule: React.FC = () => {
+        const [timetable, setTimetable] = useState<Classroom[]>([]);
+        const [loading, setLoading] = useState<boolean>(false);
+
+        const GetData = (link: string) => {
+            setLoading(true);
+            fetch(link)
+            .then((res) => {return res.json().then((result)=> {
+              setTimetable(result); console.log(result);
+              setTimeout(()=>setLoading(false), 1500);
+            })})
+          };
+        useEffect(()=>{
+        GetData('https://raw.githubusercontent.com/ketren30/english/main/english/src/components/schedule/schedule.json');
+        console.log(timetable);
+        }, []);
+
+        const table = 
+        <table>
+            <>
+            <th>Time</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesdayday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturdayday</th>
+            {timetable.map((item)=> {
+                <tr>
+                    <td colSpan={7}>{Object.keys(item)[0]}</td>
+                </tr>
+            console.log(Object.entries(item)[0])
+            /* for (var prop in Object.entries(item)[1]) {
+                <tr>
+                    <td></td>
+                </tr>
+            } */
+
+        })}
+        </>
+        </table>
     return (
-        <h1>Расписание</h1>
+        <div>
+            <>
+                {loading && <h3>Loading...</h3>}
+                {!loading && timetable.length!==0 && {table}}
+            </>
+        </div>
     )
 }
