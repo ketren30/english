890e7775edb4157d905c9bf18e3e257758c156c6
ekaterina <uragan.ketren@gmail.com>
2022-08-12 12:08:54 +0300

    photos

--- english/src/components/news/news.tsx
+++ english/src/components/news/news.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as types from '../../type';
+import { SwiperSlide } from 'swiper/react';
 
-type Props = {
-    name?: string
-}
-export const News: React.FC<Props> = ({name}) => {
+
+export const News: React.FC = () => {
+    const isLogged = useSelector ((state: types.MainState) => state.logging.isLogged);
     return (
-        <h1>Новости</h1>
+       
     )
 }
